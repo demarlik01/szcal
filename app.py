37e67881aea8a7e2f3a160c4429b809bd8857cb5
demarlik01 <hyoseockkim@gmail.com>
2017-07-11 09:36:30 +0900

    Avoid exceptional conditions

--- app.py
+++ app.py
@@ -2,24 +2,35 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import Response
+from flask_cors import CORS
 from database import session
 from models import Address
 from urllib import parse
+from utils import replace_if_short_address
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/cj/secluded_place')
 def cj_secluded_place():
-    address = parse.unquote(request.args.get('address')).replace(' ', '')
-    result = session.query(Address).filter(Address.trimmed_address == address).first()
-
-    if result is not None:
-        result_dict = {
-            'zipcode': result.zipcode,
-            'address': result.address,
-            'additional_fee': result.add_fee,
-        }
-        return jsonify(result_dict)
+    address_args = request.args.get('address')
+    if address_args:
+        trimmed_address = parse.unquote(address_args).replace(' ', '')
+        address = replace_if_short_address(trimmed_address)
+        result = session.query(Address).filter(Address.trimmed_address == address).first()
+        if result is not None:
+            result_dict = {
+                'zipcode': result.zipcode,
+                'address': result.address,
+                'additional_fee': result.add_fee,
+            }
+            return jsonify(result_dict)
+        else:
+            return Response(status=404)
     else:
-        return Response(status=404)
+        return Response(status=400)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
